7c9a533507f60b265a8eb16617083a4e324991d4
Shakir Ali <shakiralihamza05@gmail.com>
2022-10-01 00:24:34 +0500

    UI updates

--- src/components/Success.tsx
+++ src/components/Success.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import {Dialog, Grid, Slide, useMediaQuery, useTheme} from "@mui/material";
+import {Box, Dialog, Grid, Slide, Stack, useMediaQuery, useTheme} from "@mui/material";
 import Paper from "@mui/material/Paper";
 import {TransitionProps} from "@mui/material/transitions";
 import DoneIcon from '@mui/icons-material/Done';
@@ -21,7 +21,6 @@ function Success({open}: { open: boolean }) {
 
     return (
         <Dialog
-
             fullScreen
             BackdropProps={{invisible: true}}
             TransitionComponent={Transition}
@@ -35,49 +34,38 @@ function Success({open}: { open: boolean }) {
         >
             <Grid container sx={{backgroundColor: 'transparent', height: '100%'}} alignItems={'center'}
                   justifyContent={'center'}>
-                <Grid item xs={12} sm={11} md={9} sx={{height: isBelowXS?'100%':'70%'}}>
+                <Grid item xs={12} sm={11} md={9} sx={{height: isBelowXS ? '100%' : '70%'}}>
                     <Paper sx={{
                         width: '100%',
                         height: '100%',
                         backgroundColor: 'primary.main',
-                        borderRadius: isBelowXS?0:4,
+                        borderRadius: isBelowXS ? 0 : 4,
                         border: 'solid white',
                         borderWidth: isBelowXS ? 0 : isBelowSM ? '3px' : '10px',
                         color: 'white'
                     }}
                     >
-                        <Grid
-                            container
-                            direction={'column'}
-                            justifyContent={'center'}
-                            alignItems={'center'}
-                            spacing={3}
-                            sx={{height: '100%'}}
-                        >
-                            <Grid item xs={'auto'}>
-                                <DoneIcon sx={{
-                                    fontSize: 60,
-                                    fontWeight: 800,
-                                    p: 4,
-                                    border: '4px solid white',
-                                    borderRadius: 50
-                                }}/>
-                            </Grid>
-                            <Grid item xs={'auto'}>
-                                <Typography variant={'h4'}>
-                                    Successful
-                                </Typography>
-                            </Grid>
-                            <Grid item xs={'auto'} sx={{color: 'grey.300', textAlign: 'center'}}>
-                                <Typography variant={'body2'} fontWeight={450}>
+                        <Stack spacing={3} sx={{height: '100%', width: '100%'}} alignItems={'center'}
+                               justifyContent={'center'}>
+                            <DoneIcon sx={{
+                                fontSize: 60,
+                                fontWeight: 800,
+                                p: 4,
+                                border: '4px solid white',
+                                borderRadius: 50
+                            }}/>
+                            <Typography variant={'h4'}>
+                                Successful
+                            </Typography>
+                            <Box sx={{textAlign: 'center'}}>
+                                <Typography variant={'body2'} fontWeight={450} px={3}>
                                     A verification email has been sent to you.
                                 </Typography>
-                                <Typography variant={'body2'} fontWeight={450}>
+                                <Typography variant={'body2'} fontWeight={450} px={3}>
                                     Please click on the link in the email to verify your account.
                                 </Typography>
-                            </Grid>
-
-                        </Grid>
+                            </Box>
+                        </Stack>
                     </Paper>
                 </Grid>
             </Grid>
